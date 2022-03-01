4802fe2519c8c1f79429f13b4a971a00cd186ecb
Duke LeTran Win10_Evo500 <daletran@ucdavis.edu>
2022-03-01 12:15:51 -0800

    adding __init__.py

---
+++ __init__.py
@@ -0,0 +1,17 @@
+"""
+SQLWrapper.py
+    |--> prompter.py
+    
+DESCRIPTION: 
+    SQLWrapper object to help manage database connections on unix server. It
+    features a config file similar to the Oracle tnsnames.ora
+
+SETUP:
+    1. Place and edit config files in ~/.mypylib/ 
+    2. Run ```chmod 600 ~/.mypylib/db_config.ini``` to protect the files
+
+Duke LeTran <duke.letran@gmail.com; daletran@ucdavis.edu>
+Research Infrastructure, IT Health Informatics, UC Davis Health
+"""
+
+from SQLWrapper import *
