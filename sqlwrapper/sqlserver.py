e2008efef46f437061bf5ecc465ffc537927b400
Duke LeTran Win10_Evo500 <duke.letran@gmail.com>
2022-10-19 18:43:45 -0700

    add sqlserver support

--- sqlwrapper/sqlserver.py
+++ sqlwrapper/sqlserver.py
@@ -18,7 +18,11 @@ class SQLServer(SQL): # level 1
     SQL Server Database Wrapper
     Set-up: authentication config
     """
-    def __init__(self, config='OMOP_DeID', schema_name='dbo', trusted='yes', opt_print=True):
+    def __init__(self,
+                 config='OMOP_DeID',
+                 schema_name='dbo',
+                 trusted='yes',
+                 opt_print=True):
         # attempt ot initizlie
         config = db_menu(PATH_TO_CONFIG, CONFIG_FILE, opt_print=opt_print).read_config(db=config) # local variable not saved
         if config is None:
@@ -40,15 +44,6 @@ class SQLServer(SQL): # level 1
         msg_closed_success = 'Both the cursor and connection are successfully closed.'
         if self.engine:
             self.engine.dispose()
-        # try:
-        #     super().__del__()
-        #     print(f'{msg_closed_success}')
-        # except pyodbc.ProgrammingError:
-        #     try:
-        #         self.conn.close()
-        #         print(f'Cursor previously closed. {msg_closed_success}')
-        #     except pyodbc.ProgrammingError:
-        #         print(f'{msg_closed_success}')
                 
     def _generate_conn_string(self, config):
         if config['world'] is None: # if windows auth (no username)
@@ -87,9 +82,14 @@ class SQLServer(SQL): # level 1
             self.prefix = db_name + '.' + self.schema_name
     
     def _generate_engine(self):
+        """
+        https://docs.sqlalchemy.org/en/20/dialects/mssql.html
+        https://stackoverflow.com/a/48861231/9335288
+        """
         self.url_conn_string = (f'mssql+pyodbc:///?odbc_connect=' \
                                          f'{self.encoded_url_string}')
-        self.engine = sqlalchemy.create_engine(self.url_conn_string)
+        self.engine = sqlalchemy.create_engine(self.url_conn_string, 
+                                             fast_executemany=True))
 
     def _generate_inspector(self):
         from sqlalchemy import inspect
@@ -101,10 +101,8 @@ class SQLServer(SQL): # level 1
         else:
             pw=config['world']
         self._generate_conn_string(config)
-        #self._generate_connection(config)
         self._generate_engine()
         self._generate_inspector()
-        #self._generate_cursor()
         print(f'New connection successfully established to: {self.prefix}')
     
     def _reconnect(self):
@@ -222,7 +220,7 @@ class SQLServer(SQL): # level 1
         # parse for number of rows
         for tbl_name in df_output['tbl_name']:
             sql_statement = f"SELECT COUNT(*) FROM {tbl_name}"
-            int_row = pd.read_sql(sql_statement, self.conn).iloc[0,0]
+            int_row = pd.read_sql(sql_statement, self.engine).iloc[0,0]
             #print(tbl_name, ', row_count:', int_row)
             ls_rowcount.append(int_row)
             
@@ -232,8 +230,8 @@ class SQLServer(SQL): # level 1
         
     def scope(self):
         print('[Current Scope]\n',
-              'Server:', self.config['SERVER'], '\n',
-              'Database:', self.config['DATABASE'], '\n', 
+              'Server:', self._config['SERVER'], '\n',
+              'Database:', self._config['DATABASE'], '\n', 
               'Schema:', self.schema_name, '\n')
 
     @staticmethod
@@ -244,6 +242,16 @@ class SQLServer(SQL): # level 1
             sql_statement = f"SELECT {col_names} FROM {prefix}.{tbl_name};"
         return sql_statement
     
+    @staticmethod
+    def _get_database(prefix, database, database_default):
+        """ check if schema is defined, else use default"""
+        if database is not None: #if database is defined
+            database = f'{database}'
+        else: # else use default
+            database = f'{database_default}'
+        prefix = f'{database}.{prefix}'
+        return prefix
+
     def count(tbl_name):
         return pd.read_sql("SELECT COUNT(*) FROM {tbl_name}.")
     
@@ -251,6 +259,7 @@ class SQLServer(SQL): # level 1
                tbl_name:str,
                cols:Union[list, str]='*',
                #cols, tbl_name,
+               database:str=None,
                schema:str=None,
                print_bool:bool=True,
                limit:int=10, # default to 10
@@ -262,6 +271,8 @@ class SQLServer(SQL): # level 1
         col_names = self._select_cols(cols) 
         # SCHEMA
         prefix = self._get_schema(schema, self.schema_name)
+        # DATABASE
+        prefix = self._get_schema(prefix, database, self.db_name)
         # LIMIT - select TOP goes in front in SQLServer, hence the 
         sql_statement = self._limit(col_names, limit, prefix, tbl_name)
         # WHERE
@@ -271,27 +282,178 @@ class SQLServer(SQL): # level 1
         # LOG
         if print_bool:
             self._save_sql_hx(sql_statement)
-            return pd.read_sql(sql_statement, self.conn)
+            return pd.read_sql(sql_statement, self.engine)
 
-    # def sql(self, sql_statement):
-    #     """For typing out whole generic SQL statements; no checks"""
-    #     pd.read_sql(sql_statement, self.conn)
-        
-    # def insert(self, tbl_name, data_values, schema=None):
-    #     pass
-    
-    def insert_csv(self, tbl_name, csv_path):
-        """inserts dataframe"""
-        df_input = pd.read_csv(csv_path)
-        self.insert_df(tbl_name, df_input)
-    
-    def insert_df(self, tbl_name, df_input, schema=None, method='multi'):
+    def tables(self, verbose=False):
+        if verbose:
+            ls_cols = [
+                'schema_name(schema_id) schema_name', 
+                'name tbl_name',
+                'type', 
+                'type_desc', 
+                'create_date', 
+                'modify_date'
+            ]
+            sql_statement = f"select {', '.join(ls_cols)} "
+            sql_statement += "FROM sys.tables"
+            sql_statement += "ORDER BY name"
+        else:
+            sql_statement = "select name FROM sys.tables ORDER BY name"
+        return list(self.read_sql(sql_statement))
+
+    def columns(self,
+                tbl_name:str,
+                verbose=False,
+                return_dtype=False) -> Union[pd.core.indexes.base.Index, list]:
+    """https://docs.sqlalchemy.org/en/14/dialects/mssql.html#sql-server-data-types"""
+        if verbose:
+            return self.inspector.get_columns(tbl_name.lower())
+        elif return_dtype:
+            print('https://docs.sqlalchemy.org/en/14/dialects/mssql.html#sql-server-data-types')
+            df_dtype = pd.DataFrame(self.inspector.get_columns(tbl_name.lower(), dialect_options='mssql'))
+            return {k.upper():v for k,v in zip(df_dtype['name'], df_dtype['type'])}
+        else:
+            df_result = self.select(tbl_name, limit=1, print_bool=False)
+            return df_result.columns
+
+    def schemas(self, verbose=False) -> list:  
+        """ returns a list of schemas, no verbose option """
+        ls_exclude = [
+            'db_accessadmin',
+            'db_backupoperator',
+            'db_datareader',
+            'db_datawriter',
+            'db_ddladmin',
+            'db_denydatareader',
+            'db_denydatawriter',
+            'db_owner',
+            'db_securityadmin',
+            'sys',
+            'INFORMATION_SCHEMA',
+            'guest'
+        ]
+        sql_statement = "SELECT name FROM sys.schemas "
+        sql_statement += f"WHERE name not in "
+        sql_statement += f"({', '.join([f"'{x}'" for x in ls_exclude])})"
+        sql_statement += "AND name not like 'HS\\%'"
+        sql_statement += "ORDER BY name"
+        return list(self.read_sql(sql_statement))
+
+
+    def databases(self):
+        """returns a list of all databases"""
+        ls_exclude = ['master','tempdb','model','msdb']
+        sql_statement = "SELECT name FROM sys.databases "
+        sql_statement += "WHERE name not in "
+        sql_statement += f"({', '.join([f"'{x}'" for x in ls_exclude])})" 
+        return list(self.read_sql(sql_statement))
+
+    def truncate(self, table:str,
+                 database:str=None,
+                 schema:str=None,
+                 engine=None,
+                 answer=None,
+                 tbl_lower=True):
+        """
+        You can use this to truncate other tables too, static method
+        """
+        # set defaults
+        if database is None:
+            database = self.db_name
         if schema is None:
-            schema=self.schema_name
-        df_input.to_sql(tbl_name,
-                        con=self.engine,
-                        method=method,
-                        schema=schema)
-        self._generate_dbinfo() #update info
+            schema = self.schema_name
+        if engine is None:
+            engine = self.engine
         
+        # prompt for confirmation
+        if not p.prompt_confirmation(answer=answer): # if user denies
+            print('Did not truncate, canceled by user.')
+
+        # table_name.lower()
+        if tbl_lower == True:
+            table = table.lower()
+
+        # create connection and truncate
+        conn = engine.raw_connection()
+        cursor = conn.cursor()
+        log.info("=======================================================")
+        log.info(f"TRUNCATE TABLE {schema}.{table}... ")
+        log.info("=======================================================")
+        cursor.execute(f"TRUNCATE TABLE {schema}.{table}")
+        log.info("Table truncated, done!")
+        conn.close()
+
+    def drop(self, tbl_name:str,
+             what:str='TABLE',
+             database:str=None,
+             schema:str=None,
+             skip_prompt=False,
+             answer=None):
+        """For now this only drops tables, will expand in future to include sequences, etc."""
+        if skip_prompt:
+            answer = 'yes'
+        if tbl_name not in self.tables():
+           print(f'Table {tbl_name} does not exist in the db. Nothing to drop.')
+           return False
+        if database is None:
+            database = self.db_name
+        if schema is None:
+            schema = self.schema_name
+
+        sql_statement = f'DROP {what} {database}.{schema}.{tbl_name}'
+        if p.prompt_confirmation(msg=f'Are you sure your want to drop {tbl_name}?', answer=answer):
+            self.read_sql(sql_statement)
+    
+    # def insert_csv(self, tbl_name, csv_path):
+    #     """inserts dataframe"""
+    #     df_input = pd.read_csv(csv_path)
+    #     self.insert_df(tbl_name, df_input)
+    
+    # def insert_df(self, tbl_name, df_input, schema=None, method='multi'):
+    #     if schema is None:
+    #         schema=self.schema_name
+    #     df_input.to_sql(tbl_name,
+    #                     con=self.engine,
+    #                     method=method,
+    #                     schema=schema)
+
+    def insert(self, df_input,
+                     table,
+                     engine=None,
+                     schema=None,
+                     cap_cols=False,
+                     index=False, # set default to False
+                     method="multi",
+                     chunksize=1000,
+                     **kwargs):
+        """
+        Since pd.DataFrame.to_sql() supports executemany() natively, this func
+        only sets some defaults to my preferences. Otherwise, it effectively
+        is the same.
+        * https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_sql.html
+        * https://pandas.pydata.org/pandas-docs/stable/user_guide/io.html#io-sql-method
+        * execute_many:https://stackoverflow.com/a/48861231/9335288
+        * https://docs.sqlalchemy.org/en/20/dialects/mssql.html#fast-executemany-mode
+        """
+        # SET DEFAULTS #########################################################
+        if cap_cols:
+            df_input.columns = [x.upper() for x in df_input.columns]
+
+        if engine is None:
+            engine = self.engine
+
+        if schema is None:
+            schema = self.schema_name
+
+        # You can use pd.DataFrame.to_sql() for SQLServer!!
+        df_input.to_sql(table,
+            engine,
+            index=index,
+            schema=schema,
+            method=method,
+            chunksize=chunksize,
+            **kwargs)
+
+        
+
 
