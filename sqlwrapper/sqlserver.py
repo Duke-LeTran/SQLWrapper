86db1d924bbddaf1373da05abf1eb63f30edcd5d
Duke LeTran Win10_Evo500 <duke.letran@gmail.com>
2022-10-21 02:05:30 -0700

    added prompter to sqlserver

--- sqlwrapper/sqlserver.py
+++ sqlwrapper/sqlserver.py
@@ -5,13 +5,14 @@ from getpass import getpass
 import pyodbc
 import pandas as pd
 from typing import Union
-
+from sqlwrapper import db_menu, PATH_TO_CONFIG, CONFIG_FILE, Prompter
 from sqlwrapper.base import SQL
-from sqlwrapper.config import PATH_TO_CONFIG, CONFIG_FILE
-from sqlwrapper.dbmenu import db_menu
 
 # logging
 log = logging.getLogger(__name__)
+
+# prompter
+p = Prompter()
 
 class SQLServer(SQL): # level 1
     """
