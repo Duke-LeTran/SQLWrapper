e3713568abfafccfd415e700d9dbd96ba80b0166
Duke <duke.letran@gmail.com>
2023-03-21 10:09:11 -0700

    fixed sqlalchemy 2.0 text breaking changes, backwards compatible

--- sqlwrapper/oracle.py
+++ sqlwrapper/oracle.py
@@ -232,7 +232,8 @@ class Oracle(SQL, parameters): # level 1
         # LOG
         if print_bool:
             self._save_sql_hx(sql_statement + ';')
-        df_output = pd.read_sql(sql_statement, con=self.engine)
+        #df_output = pd.read_sql(sql_statement, con=self.engine)
+        df_output = self.read_sql(sql_statement)
         # convert names to capital for consistency
         df_output.columns = [x.upper() for x in df_output.columns]
         return df_output
